70ef32abcd9d7ebc835875bf51908afe189c894d
Roshan Bohara <bohararoshan911@gmail.com>
2024-07-03 17:05:20 +0545

    first commit

---
+++ day_one.py
@@ -0,0 +1,28 @@
+# print("Hello World")
+# name = 'Ram'
+# print(type(name))
+# Age = 20
+# print(type(Age))
+# Name = input("What is Your Name")
+# print("My name is " + Name)
+
+
+# # WAP to create an app that can calculate user's age bi its date of birth
+# year_of_birth = input("Enter your year of birth")
+# current_year = 2024
+# current_age = current_year - int(year_of_birth)
+
+# print("Your current age is: ")
+# print(current_age)
+
+# # Using Condition
+# if current_age < 18:
+#     print("You are not eligible to enter")
+# else:
+#     print("Yes you can enter the hall")
+
+
+
+
+name = input("ENter Your Name")
+print(name)
